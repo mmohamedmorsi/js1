e701a2c9a766e9299efc605d5f4fe1958319ddfe
mmohamedmorsi <mohmor.abdo@gmail.com>
2024-12-29 07:58:10 -0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("demo").innerHTML ="Date & time"
+document.getElementById('bt-on').onclick =function (){
+    document.getElementById('lamp').src ='img/2.gif'
+    
+} ;
+document.getElementById('bt-off').onclick =function(){
+
+    document.getElementById('lamp').src='img/1.gif'
+
+}; 
+
